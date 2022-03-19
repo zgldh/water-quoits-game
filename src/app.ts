fdc9da21cbe227cc319026c7b0f1cd5b3027be1c
zgldh <zgldh@hotmail.com>
2022-03-20 02:03:05 +0800

    box and sphere

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, StandardMaterial, Texture } from "@babylonjs/core";
-import { BOX_BORDER, BOX_DEEPTH, BOX_HEIGHT, BOX_WIDTH } from "./consts";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, StandardMaterial, Texture, CannonJSPlugin, IPhysicsEnabledObject, IPhysicsEngine, PhysicsImpostor, BoxBuilder, AmmoJSPlugin } from "@babylonjs/core";
+import { BOX_BORDER, BOX_DEEPTH, BOX_HEIGHT, BOX_WIDTH, MAX_SPHERES, SPHERE_DIAMETER } from "./consts";
 
 class App {
     private canvas: HTMLCanvasElement;
@@ -10,32 +10,10 @@ class App {
     private scene!: Scene;
     private camera!: ArcRotateCamera;
 
-    private prepareScene() {
-        this.scene = new Scene(this.engine);
+    private physicsEngine!: IPhysicsEngine;
 
-        this.camera = new ArcRotateCamera("Camera", 0, 0, 10, Vector3.Zero(), this.scene);
-        this.camera.position = new Vector3(0, 0, -10);
-        this.camera.attachControl(this.canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this.scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 0.5 }, this.scene);
+    private currentOrientationGamma: number = 0;
 
-        // Play box
-        const wallLeft = MeshBuilder.CreateBox("wallLeft", { width: BOX_BORDER, height: BOX_HEIGHT - BOX_BORDER * 2, depth: BOX_DEEPTH });
-        const wallRight = MeshBuilder.CreateBox("wallRight", { width: BOX_BORDER, height: BOX_HEIGHT - BOX_BORDER * 2, depth: BOX_DEEPTH });
-        const wallTop = MeshBuilder.CreateBox("wallTop", { width: BOX_WIDTH, height: BOX_BORDER, depth: BOX_DEEPTH });
-        const wallBottom = MeshBuilder.CreateBox("wallBottom", { width: BOX_WIDTH, height: BOX_BORDER, depth: BOX_DEEPTH });
-        const frontWall = MeshBuilder.CreateBox("frontWall", { width: BOX_WIDTH, height: BOX_HEIGHT, depth: BOX_DEEPTH });
-        const backWall = MeshBuilder.CreateBox("backWall", { width: BOX_WIDTH, height: BOX_HEIGHT, depth: BOX_DEEPTH });
-
-        wallLeft.position.x = -BOX_WIDTH / 2 + BOX_BORDER / 2;
-        wallRight.position.x = BOX_WIDTH / 2 - BOX_BORDER / 2;
-        wallTop.position.y = BOX_HEIGHT / 2 - BOX_BORDER / 2;
-        wallBottom.position.y = -BOX_HEIGHT / 2 + BOX_BORDER / 2;
-        frontWall.position.z = -BOX_DEEPTH;
-        backWall.position.z = BOX_DEEPTH;
-
-        frontWall.visibility = 0.2;
-    }
 
     constructor() {
         // create the canvas html element and attach it to the webpage
@@ -48,8 +26,62 @@ class App {
         // initialize babylon scene and engine
         this.engine = new Engine(this.canvas, true);
         this.prepareScene();
-        this.scene.debugLayer.show();
+        // this.scene.debugLayer.show();
 
+        this.prepareInput();
+
+        // run the main render loop
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        });
+    }
+    private prepareScene() {
+        this.scene = new Scene(this.engine);
+        var gravityVector = new Vector3(0, -9.81, 0);
+        // var physicsPlugin = new CannonJSPlugin();
+        var physicsPlugin = new AmmoJSPlugin();
+        this.scene.enablePhysics(gravityVector, physicsPlugin);
+        this.physicsEngine = this.scene.getPhysicsEngine() as IPhysicsEngine;
+
+        this.camera = new ArcRotateCamera("Camera", 0, 0, 10, Vector3.Zero(), this.scene);
+        this.camera.position = new Vector3(0, 0, -10);
+        this.camera.attachControl(this.canvas, true);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this.scene);
+
+        // Play box
+        const wallLeft = BoxBuilder.CreateBox("wallLeft", { width: BOX_BORDER, height: BOX_HEIGHT - BOX_BORDER * 2, depth: BOX_DEEPTH });
+        const wallRight = BoxBuilder.CreateBox("wallRight", { width: BOX_BORDER, height: BOX_HEIGHT - BOX_BORDER * 2, depth: BOX_DEEPTH });
+        const wallTop = BoxBuilder.CreateBox("wallTop", { width: BOX_WIDTH, height: BOX_BORDER, depth: BOX_DEEPTH });
+        const wallBottom = BoxBuilder.CreateBox("wallBottom", { width: BOX_WIDTH, height: BOX_BORDER, depth: BOX_DEEPTH });
+        const frontWall = BoxBuilder.CreateBox("frontWall", { width: BOX_WIDTH, height: BOX_HEIGHT, depth: BOX_DEEPTH });
+        const backWall = BoxBuilder.CreateBox("backWall", { width: BOX_WIDTH, height: BOX_HEIGHT, depth: BOX_DEEPTH });
+
+        wallLeft.position.x = -BOX_WIDTH / 2 + BOX_BORDER / 2;
+        wallRight.position.x = BOX_WIDTH / 2 - BOX_BORDER / 2;
+        wallTop.position.y = BOX_HEIGHT / 2 - BOX_BORDER / 2;
+        wallBottom.position.y = -BOX_HEIGHT / 2 + BOX_BORDER / 2;
+        frontWall.position.z = -BOX_DEEPTH;
+        backWall.position.z = BOX_DEEPTH;
+
+        wallLeft.physicsImpostor = new PhysicsImpostor(wallLeft, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, this.scene);
+        wallRight.physicsImpostor = new PhysicsImpostor(wallRight, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, this.scene);
+        wallTop.physicsImpostor = new PhysicsImpostor(wallTop, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, this.scene);
+        wallBottom.physicsImpostor = new PhysicsImpostor(wallBottom, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, this.scene);
+        frontWall.physicsImpostor = new PhysicsImpostor(frontWall, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, this.scene);
+        backWall.physicsImpostor = new PhysicsImpostor(backWall, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, this.scene);
+
+        frontWall.visibility = 0.2;
+
+        // Spheres
+        for (let i = 0; i < MAX_SPHERES; i++) {
+            let sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: SPHERE_DIAMETER }, this.scene);
+            sphere.position.x = Math.random() * (BOX_WIDTH - BOX_BORDER * 2) - (BOX_WIDTH - BOX_BORDER * 2) / 2;
+            sphere.position.y = Math.random() * (BOX_HEIGHT - BOX_BORDER * 2) - (BOX_HEIGHT - BOX_BORDER * 2) / 2;
+            sphere.physicsImpostor = new PhysicsImpostor(sphere, PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, this.scene);
+        }
+
+    }
+    private prepareInput() {
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
@@ -62,10 +94,21 @@ class App {
             }
         });
 
-        // run the main render loop
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-        });
+        window.addEventListener('deviceorientation', (event: DeviceOrientationEvent) => {
+            this.currentOrientationGamma = event.gamma;
+            this.engine.resize();
+        }, true);
+
+        window.addEventListener('devicemotion', event => {
+            if (event.accelerationIncludingGravity.y !== null) {
+                const gravityVector = new Vector3(
+                    event.accelerationIncludingGravity.y,
+                    -event.accelerationIncludingGravity.x,
+                    event.accelerationIncludingGravity.z);
+                this.physicsEngine.setGravity(gravityVector);
+            }
+        }, true);
+
     }
 }
 new App();
--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,6 @@ export const BOX_WIDTH = 18;
 export const BOX_HEIGHT = 9;
 export const BOX_DEEPTH = 1;
 export const BOX_BORDER = 0.5;
+
+export const MAX_SPHERES = 200;
+export const SPHERE_DIAMETER = 0.7;
