75d0c07bcd0f1651dde001a685b150737bd3c71c
zgldh <zgldh@hotmail.com>
2022-04-01 00:35:16 +0800

    cordova orientation

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,38 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
-    output: {
-        filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
-        clean: true,
+  entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
+  output: {
+    path: __dirname + "/cordova/www", //path to the output directory
+    filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080, //port that we're using for local host (localhost:8080)
+    static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+    hot: true,
+    devMiddleware: {
+      publicPath: "/",
     },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    devServer: {
-        host: "0.0.0.0",
-        port: 8080, //port that we're using for local host (localhost:8080)
-        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
-        hot: true,
-        devMiddleware: {
-            publicPath: "/",
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve(appDirectory, "public/index.html"),
-        })
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-    mode: "development",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(appDirectory, "public/index.html"),
+    }),
+  ],
+  mode: "development",
 };
